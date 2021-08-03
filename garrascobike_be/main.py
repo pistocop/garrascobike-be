d063f4a86ae3067f198022b57ede7e9fb06bd7df
pistocop <simone.guardati42@gmail.com>
2021-08-03 08:41:15 +0200

    :sparkles: CORS management backbone and Server reload on changes

--- garrascobike_be/main.py
+++ garrascobike_be/main.py
@@ -6,6 +6,7 @@ from dotenv import find_dotenv
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from loguru import logger
+from fastapi.middleware.cors import CORSMiddleware
 
 from garrascobike_be.submodules.knn_manager import KnnManager
 from garrascobike_be.utils.backblaze import download_garrascobike_model
@@ -18,10 +19,23 @@ local_model_path = download_garrascobike_model(local_path=local_models_folder,
                                                model_info_path=models_host_info,
                                                app_key_id=os.getenv("BB_APP_KEY_ID"),
                                                app_key=os.getenv("BB_APP_KEY"))
+
+origins = [
+    "http://localhost:8888",
+    "https://localhost:8888",
+]
+
 app = FastAPI(
     title="garrascobike-be",
     version="1.0",
     description="Back-end services for garrascobike App",
+)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 knn_mng = KnnManager()
 knn_mng.load_model(local_model_path)
@@ -41,7 +55,8 @@ def recommender(bike_name: str):
         for score, bike in suggestions:
             results.append({"score": score, "bike": bike})
         return results
-    except Exception:
+    except Exception as e:
+        logger.warning(f"Exception: {e}")
         msg = f"Bike {bike_name} not found"
         logger.info(msg)
         return msg
@@ -49,4 +64,4 @@ def recommender(bike_name: str):
 
 if __name__ == "__main__":
     # noinspection PyTypeChecker
-    uvicorn.run(app, host="0.0.0.0", port=8888)
+    uvicorn.run("garrascobike_be.main:app", host="0.0.0.0", port=8888, reload=True)
